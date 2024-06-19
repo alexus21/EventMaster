b849f8bd47cd4ada6851f69bc6f7132a4f0fa78c
Alex Ulloa <US21003@ues.edu.sv>
2024-06-19 11:13:28 -0600

    Corrección de errores; conexión a firebase

--- app/src/main/java/ues/pdm24/eventmaster/activities/LoginActivity.java
+++ app/src/main/java/ues/pdm24/eventmaster/activities/LoginActivity.java
@@ -13,13 +13,12 @@ import ues.pdm24.eventmaster.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.LoginActivity), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
